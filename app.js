13ce820ef637b9ce0fac467c255018bb37335f3b
Nurlykhan Kakpan <nurlykhan.kakpan@hardcode.kz>
2024-02-29 21:52:04 +0600

    update

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const authService = require('./services/authService');
 const historicalEventService = require('./services/hisEventsService');
 const hisFigureService = require('./services/hisFigureService');
 const userService = require('./services/userService');
+const jwt = require('jsonwebtoken');
+const Item = require('./models/item');
 const app = express();
 
 // Connect to MongoDB Atlas
@@ -45,6 +47,7 @@ app.post('/login', async (req, res) => {
     const user = req.body;
     try {
       const isAdmin = await authService.login(user);
+      req.session.token = jwt.sign({isAdmin}, 'my-secret-key', {expiresIn: 60 * 60});
       if(isAdmin){
         res.redirect('/admin');
       }
@@ -66,7 +69,7 @@ app.post('/register', async (req, res) => {
   try {
     const result = await authService.register(user);
     if(result){
-      res.redirect('/home');
+      res.redirect('/login');
     }
   }
   catch (error) {
@@ -74,7 +77,7 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.get('/home', async (req, res) => {
+app.get('/home', authMiddleware, async (req, res) => {
   const text = req.query.text || 'kazakhstan';
   try {
     const events = await historicalEventService.getEvents(text);
@@ -85,7 +88,7 @@ app.get('/home', async (req, res) => {
   }
 });
 
-app.get('/hisEvents', async (req, res) => {
+app.get('/hisEvents',authMiddleware, async (req, res) => {
     const text = req.query.text || 'kazakhstan';
     try {
         const events = await historicalEventService.getEvents(text);
@@ -96,7 +99,7 @@ app.get('/hisEvents', async (req, res) => {
     }
 })
 
-app.get('/figures', async (req, res) => {
+app.get('/figures',authMiddleware, async (req, res) => {
   const name = req.query.name || 'cesar';
     try {
       const figures = await hisFigureService.getFigures(name);
@@ -112,7 +115,7 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
-app.get('/admin', async (req,res) =>{
+app.get('/admin',authMiddleware, async (req,res) =>{
     try {
         const users= await userService.getUsers();
         res.render('admin', { users });
@@ -131,7 +134,7 @@ app.post('/admin/delete', async (req, res) => {
         res.redirect('/admin');
     }
 })
-app.post('/admin', async (req, res) => {
+app.post('/admin', authMiddleware, async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     const isAdmin = req.body.isAdmin === 'on';
@@ -141,6 +144,65 @@ app.post('/admin', async (req, res) => {
     }
     catch (error) {
         res.redirect('/admin');
+    }
+});
+function authMiddleware(req, res, next) {
+    // Check if the token exists in the session
+    if (req.session && req.session.token) {
+        // Token exists, so user is authenticated
+        next(); // Continue to the next middleware or route handler
+    } else {
+        // Token doesn't exist, so user is not authenticated
+        res.render('index', {message: 'You have not token, you need first login'})// Respond with 401 Unauthorized
+    }
+}
+app.post('/add-item', authMiddleware,async (req, res) => {
+    const { pictures, name1, name2, description1, description2 } = req.body;
+
+    const newItem = new Item({
+        pictures: pictures.split(',').map(url => url.trim()),
+        names: [{ lang: 'en', name: name1 }, { lang: 'es', name: name2 }],
+        descriptions: [{ lang: 'en', description: description1 }, { lang: 'es', description: description2 }]
+    });
+
+    try {
+        await newItem.save();
+        res.redirect('/admin'); // Redirect to admin page after adding the item
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error adding item');
+    }
+});
+
+app.get('/items', authMiddleware,async (req, res) => {
+    try {
+        const items = await Item.find();
+        res.render('itemsList', { items });
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+app.get('/items-add-page', authMiddleware, (req, res) => {
+    res.render('ItemAdd');
+})
+app.get('/items-for-admin',authMiddleware, async (req, res) => {
+    try {
+        const items = await Item.find();
+        res.render('itemsListAdmin', { items });
+    } catch (error) {
+        console.error(error);
+    }
+});
+// Route to handle item deletion
+app.post('/delete-item/:id', authMiddleware,async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Item.findByIdAndDelete(id);
+        res.redirect('/items-for-admin');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error deleting item');
     }
 });
 
